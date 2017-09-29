90501bd335856deb21a63627c7bc3e9b0088db26
DocHackberry <abryan81@gmail.com>
2017-09-29 20:37:21 +0000

    Began adding web version

---
+++ card.js
@@ -0,0 +1,17 @@
+var Suit = ["Hearts", "Diamonds", "Clubs", "Spades"];
+var Rank = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+var card = class Card{
+    constructor(){
+        this.suit = null;
+        this.rank = null;
+    }
+
+    Display_Card(){}
+    Draw_Card (){}
+    Set_Rank (newRank){}
+	Set_Suit (newSuit){}
+	Get_Value (){}
+	Draw_Back (){}
+	
+}
